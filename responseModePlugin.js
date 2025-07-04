28538c32653077200c59f715aaf3b4b90871d4dd
Nicolas Levy <nicolas@qualabs.com>
2025-07-04 17:57:09 -0300

    fixes for old shaka plaeyrs, added new cmcd v2 keys

--- responseModePlugin.js
+++ responseModePlugin.js
@@ -2,12 +2,26 @@
     const _enableResponseMode = (player, config) => {
         const currentMode = config.mode || 'json';
         const cmcdBatchArray = (currentMode === 'json') ? [] : null;
+        const videoElement = player.getMediaElement();
+
+        let sequenceNumber = 0;
+        let timePlay = null;
+        let msd = null;
+        let msdSent = false;
+
+        videoElement.addEventListener('play', function () {
+            if (timePlay == null) timePlay = new Date().getTime();
+        });
+
+        videoElement.addEventListener('playing', function () {
+            if (msd == null) msd = new Date().getTime() - timePlay;
+        });
 
         function sendCmcdReport(cmcdData, reportingUrl) {
             if (currentMode == 'json'){
                 if (!cmcdData || cmcdData.length === 0) return;
         
-                console.log(`Sending batch of ${cmcdData.length} CMCD reports.`);
+                // console.log(`Sending batch of ${cmcdData.length} CMCD reports.`);
                 fetch(reportingUrl, {
                     method: 'POST',
                     headers: {
@@ -16,7 +30,7 @@
                     body: JSON.stringify(cmcdData),
                 })
                 .then(reportResponse => {
-                    console.log(reportResponse.ok ? 'CMCD batch data reported successfully.' : `Reporting server responded with an error.`);
+                    // console.log(reportResponse.ok ? 'CMCD batch data reported successfully.' : `Reporting server responded with an error.`);
                 })
                 .catch(error => {
                     console.error('Error sending CMCD batch data to reporting server:', error);
@@ -24,17 +38,38 @@
             } else if (currentMode === 'query') {
                 if (!cmcdData) return;
                 
-                console.log(`Sending CMCD report via query: ${reportingUrl.toString()}`);
+                // console.log(`Sending CMCD report via query: ${reportingUrl.toString()}`);
                 fetch(reportingUrl, {
                     method: 'GET',
                 })
                 .then(reportResponse => {
-                    console.log(reportResponse.ok ? 'CMCD query data reported successfully.' : `Reporting server responded with an error`);
+                    // console.log(reportResponse.ok ? 'CMCD query data reported successfully.' : `Reporting server responded with an error`);
                 })
                 .catch(error => {
                     console.error('Error sending CMCD query data to reporting server:', error);
                 });
             }
+        }
+
+        // TODO: May need some debugging
+        function getPlayerState(player) {
+            const video = player.getMediaElement();
+            if (!video) return;
+            if (video.seeking) return 'k';
+            if (player.isBuffering()) return 'r';
+            if (video.ended) return 'e'
+            // 4. Paused / Preloading
+            if (video.paused) {
+                if (video.currentTime === 0 && video.played.length === 0) {
+                    return 'p'; // Prealoading
+                }
+                return 'a'; //Paused
+            }
+            // 5. Playing / Starting
+            if (video.readyState < 3) {
+                return 's';
+            }
+            return 'p';
         }
 
         // Add a response filter to process CMCD data and send a report.
@@ -79,76 +114,165 @@
                     let key, value;
 
                     const { originalRequest, timeMs } = response;
+
+                    // Some old versions of Shaka Player do not have originalRequest, so we need to check for it.
                     if (originalRequest) {
                         
                         // Add 'ts' (timestamp) CMCD key: Time request was initiated (epoch ms).
                         key = 'ts';
-                        if (typeof originalRequest.requestStartTime === 'number' && (config.includeKeys === undefined || config.includeKeys.includes(key))) {
-                            value = Math.round(originalRequest.requestStartTime);
-                            cmcdJsonObjectForBody[key] = value;
-                            newCmcdPairs.push(`${key}=${value}`);
+                        if (!cmcdJsonObjectForBody[key]) {
+                            if (typeof originalRequest.requestStartTime === 'number' && (config.includeKeys === undefined || config.includeKeys.includes(key))) {
+                                value = Math.round(originalRequest.requestStartTime);
+                                cmcdJsonObjectForBody[key] = value;
+                                newCmcdPairs.push(`${key}=${value}`);
+                            }
                         }
 
                         // Add 'ttfb' (Time To First Byte) CMCD key.
                         key = 'ttfb';
-                        if (typeof originalRequest.timeToFirstByte === 'number' && (config.includeKeys === undefined || config.includeKeys.includes(key))) {
-                            value = Math.round(originalRequest.timeToFirstByte);
-                            cmcdJsonObjectForBody[key] = value;
-                            newCmcdPairs.push(`${key}=${value}`);
+                        if (!cmcdJsonObjectForBody[key]) {
+                            if (typeof originalRequest.timeToFirstByte === 'number' && (config.includeKeys === undefined || config.includeKeys.includes(key))) {
+                                value = Math.round(originalRequest.timeToFirstByte);
+                                cmcdJsonObjectForBody[key] = value;
+                                newCmcdPairs.push(`${key}=${value}`);
+                            }
                         }
+                    } else {
+                        // For older versions of Shaka Player, we can add 'ts' key based on the response time of the response
+                        key = 'ts';
+                        if (!cmcdJsonObjectForBody[key]) {
+                            if (timeMs && typeof timeMs === 'number' && (config.includeKeys === undefined || config.includeKeys.includes(key))) {
+                                value = (new Date()).getTime() - timeMs;
+                                cmcdJsonObjectForBody[key] = value;
+                                newCmcdPairs.push(`${key}=${value}`);
+                            }
+                        }
+                    }
 
-                        // Add 'ttlb' (Time To Last Byte) CMCD key: Duration from request start to last byte.
-                        key = 'ttlb';
+                    // Add 'ttlb' (Time To Last Byte) CMCD key: Duration from request start to last byte.
+                    key = 'ttlb';
+                    if (!cmcdJsonObjectForBody[key]) {
                         if (timeMs && typeof timeMs === 'number' && (config.includeKeys === undefined || config.includeKeys.includes(key))) {
                             value = Math.round(timeMs);
                             cmcdJsonObjectForBody[key] = value;
                             newCmcdPairs.push(`${value}=${value}`);
-                        }
+                        }                    
                     }
 
                     // Add 'rc' (Response code) CMCD key: Response code of the HTTP Request.                       
                     key = 'rc';
-                    if (response && response.statusCode && (config.includeKeys === undefined || config.includeKeys.includes(key))) {
-                        value = response.statusCode
-                        cmcdJsonObjectForBody[key] = value;
-                        newCmcdPairs.push(`${key}=${value}`);
-                    } 
+                    if (!cmcdJsonObjectForBody[key]) {
+                        if (response && response.status && (config.includeKeys === undefined || config.includeKeys.includes(key))) {
+                            value = response.status
+                            cmcdJsonObjectForBody[key] = value;
+                            newCmcdPairs.push(`${key}=${value}`);
+                        } 
+                    }
 
                     // Add the 'url' (URL) CMCD key
                     key = 'url';
-                    value = response.uri;
-                    cmcdJsonObjectForBody[key] = value;
-                    const escapedUrlValue = value.replace(/\\/g, '\\\\').replace(/"/g, '\\"');
-                    newCmcdPairs.push(`${key}="${escapedUrlValue}"`);
+                    if (!cmcdJsonObjectForBody[key]) {
+                        value = response.uri.split('?')[0]; // Remove query parameters from the URL (tokens, CMCD, etc.)
+                        cmcdJsonObjectForBody[key] = value;
+                        const escapedUrlValue = value.replace(/\\/g, '\\\\').replace(/"/g, '\\"');
+                        newCmcdPairs.push(`${key}="${escapedUrlValue}"`);
+                    }
 
                     
                     // Add the 'pt' (Playhead time) CMCD key: Current playhead time in seconds if VOD or time if Live.
                     // Important: 'pt' key should be calculated at the request, not response, but we do it here for simplicity but adding some error.
                     key = 'pt';
-                    if (player.isLive()) {
-                        value = player.getPlayheadTimeAsDate().getTime();
-                    } else {
-                        value = player.getMediaElement().currentTime
-                    }
-                    cmcdJsonObjectForBody[key] = value;
-                    newCmcdPairs.push(`${key}=${value}`);
-
-                    // Add the 'ltc' (Live Latency) CMCD key if not found for live streams.
-                    // Important: 'ltc' key should be calculated at the request, not response, but we do it here for simplicity but adding some error.
-                    key = 'ltc';
-                    if (player.isLive()) {
-                        if (!cmcdJsonObjectForBody[key]) {
-                            console.log( 'Adding live latency to CMCD data');
-                            value = player.getStats().liveLatency * 1000;
+                    if (!cmcdJsonObjectForBody[key]) {
+                        if (player.isLive()) {
+                            value = player.getPlayheadTimeAsDate().getTime();
+                        } else {
+                            value = player.getMediaElement().currentTime
+                        }
+                        if (value > 0) {
                             cmcdJsonObjectForBody[key] = value;
                             newCmcdPairs.push(`${key}=${value}`);
                         }
                     }
 
+                    // Add the 'ltc' (Live Latency) CMCD key if not found for live streams.
+                    // Important: 'ltc' key should be calculated at the request, not response, but we do it here for simplicity but adding some error.
+                    key = 'ltc';
+                    if (!cmcdJsonObjectForBody[key]) {
+                        if (player.isLive()) {
+                            value = Math.round(player.getStats().liveLatency * 1000);
+                            if ( value > 0){
+                                cmcdJsonObjectForBody[key] = value;
+                                newCmcdPairs.push(`${key}=${value}`);
+                            }
+                        }
+                    }
+
+                    // Add the 'pr' (Playback Rate) CMCD key if not found
+                    // Important: 'pr' key should be calculated at the request, not response, but we do it here for simplicity but adding some error.
+                    key = 'pr';
+                    if (!cmcdJsonObjectForBody[key]) {
+                        value = player.getPlaybackRate();
+                        cmcdJsonObjectForBody[key] = value;
+                        newCmcdPairs.push(`${key}=${value}`);
+                    }
+
+                    // Add the 'sta' (Live Latency) CMCD key if not found.
+                    // Important: 'sta' key should be calculated at the request, not response, but we do it here for simplicity but adding some error.
+                    key = 'sta';
+                    if (!cmcdJsonObjectForBody[key]) {
+                        value = getPlayerState(player);
+                        cmcdJsonObjectForBody[key] = value;
+                        newCmcdPairs.push(`${key}=${value}`);
+                    }                  
+
+                    // Add the 'msd' (Media Start Delay) CMCD key if not found.
+                    // 'msd' must be sent only once per session, so we check if it was already sent.
+                    key = 'msd';
+
+                    // Remove 'msd' from the CMCD object if it was already sent by this plugin.
+                    if (cmcdJsonObjectForBody[key] && msdSent){
+                        delete cmcdJsonObjectForBody[key]
+                    }
+                    
+                    // Fund 'msd' by native CMCD implementation, setting as sent.
+                    if (cmcdJsonObjectForBody[key]){
+                        msdSent = true;
+                    }
+
+                    // 'msd' was not sent and we have a valid msd value, so we send it.               
+                    if (!msdSent && msd) {
+                        value = msd;
+                        msdSent = true;
+                        cmcdJsonObjectForBody[key] = value;
+                        newCmcdPairs.push(`${key}=${value}`);
+                    }
+                    
+                    // Droped frames ('df') is an absolute count of dropped frames since session initiation. 
+                    // This key should only be sent for content types of 'v','av' or 'o'      
+                    key = 'df'
+                    if (!cmcdJsonObjectForBody[key]) {
+                        if (videoElement.getVideoPlaybackQuality){
+                            const value = videoElement.getVideoPlaybackQuality().droppedVideoFrames;
+                            cmcdJsonObjectForBody[key] = value;
+                            newCmcdPairs.push(`${key}=${value}`);                            
+                        }
+                    }
+
+                    // Sequence Number ('sn') A monotonically increasing integer to identify the sequence of a CMCD report to a target 
+                    // within a session. This MUST be reset to zero on the start of a new session-id. 
+                    // Sequence numbers increase independently per each combination of mode and target.
+                    key = 'sn'
+                    if (!cmcdJsonObjectForBody[key]) {
+                        value = sequenceNumber;
+                        cmcdJsonObjectForBody[key] = value;
+                        newCmcdPairs.push(`${key}=${value}`);
+                        sequenceNumber = sequenceNumber + 1;                           
+                    }
+
                     const reportUrl = new URL(config.url);
                     if (currentMode == 'json'){
                         cmcdBatchArray.push(cmcdJsonObjectForBody);
-                        console.log(`CMCD data added to batch. Current batch size: ${cmcdBatchArray.length}. Data:`, JSON.stringify(cmcdJsonObjectForBody));
+                        // console.log(`CMCD data added to batch. Current batch size: ${cmcdBatchArray.length}. Data:`, JSON.stringify(cmcdJsonObjectForBody));
                         
                         if (cmcdBatchArray.length >= config.batchSize) {
                             sendCmcdReport(cmcdBatchArray.slice(), reportUrl); // Send a copy of the current batch
@@ -156,7 +280,7 @@
                         }
                     } else if (currentMode == 'query'){
                         cmcdDataString += newCmcdPairs.join(',');
-                        console.log('Response Filter - CMCD data for reporting:', cmcdDataString);
+                        // console.log('Response Filter - CMCD data for reporting:', cmcdDataString);
                         reportUrl.searchParams.set('CMCD', cmcdDataString);
                         sendCmcdReport(cmcdDataString, reportUrl);
                     }
